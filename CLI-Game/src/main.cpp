12f80160d146cf96540f2d8fb134e3c6e013300a
LuFrei <32626445+LuFrei@users.noreply.github.com>
2023-10-12 14:22:22 -0400

    Key Display
    Blocks now appear on the terminal based on what key is pressed.
    
    Bugs:
    Unexpected behavior: Blocks are toggling on and off with key press.
    Expectation behavior: is for blocks to appear when the key is pressed down, and disappear when the key is let go.

--- CLI-Game/src/main.cpp
+++ CLI-Game/src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <Windows.h>
 
-struct Vector2 {
+struct Block {
     int x, y;
+    int width, height;
 };
 
 enum KeyStatus {
@@ -13,28 +14,45 @@ enum KeyStatus {
 
 KeyStatus GetKeyInput(int key) {
     switch (GetKeyState(key)) {
-    case(0):
-        return KeyStateUp;
     case(1):
         return KeyStateToggled;
     case(0x8000):
         return KeyStateDown;
+    case(0):
+        return KeyStateUp;
     }
 }
 
-void FillBlock(Vector2 position, int width, int height, char character) {
+void FillBlock(Block block, char character) {
 
-    for (short i = 0; i < height; i++) {
+    for (short i = 0; i < block.height; i++) {
         SetConsoleCursorPosition(
             GetStdHandle(STD_OUTPUT_HANDLE),
-            { (short)position.x, (short)(position.y + i) }
+            { (short)block.x, (short)(block.y + i) }
         );
-        for (int j = 0; j < width; j++) {
+        for (int j = 0; j < block.width; j++) {
             std::cout << character;
         }
     }
 
 }
+
+
+void DrawBlockBasedOnInput(KeyStatus key, Block block, char texture) {
+    FillBlock(
+        block,
+        key == KeyStateUp ? ' ' : texture
+    );
+}
+
+// Blocks will be 4x4 for now.
+const short width = 3;
+const short height = 2;
+
+Block blockN = {9, 6, width, height};
+Block blockW = {4, 9, width, height};
+Block blockS = {9, 9, width, height};
+Block blockE = {14, 9, width, height};
 
 int main()
 {
@@ -43,24 +61,47 @@ int main()
     int count = 0;
 
     while (running) {
+        count++;
         SetConsoleCursorPosition(
             GetStdHandle(STD_OUTPUT_HANDLE),
             { 0, 0 }
         );
-        count++;
-
         std::cout << "Running... Cycle: " << count;
 
-        // Get key presses 
+        // === Get key presses === 
         KeyStatus spacebar = GetKeyInput(VK_SPACE);
+
+        KeyStatus upArrow = GetKeyInput(VK_UP);
+        KeyStatus leftArrow = GetKeyInput(VK_LEFT);
+        KeyStatus downArrow = GetKeyInput(VK_DOWN);
+        KeyStatus rightArrow = GetKeyInput(VK_RIGHT);
+
+        KeyStatus wKey = GetKeyInput(87);
+        KeyStatus aKey = GetKeyInput(65);
+        KeyStatus sKey = GetKeyInput(83);
+        KeyStatus dKey = GetKeyInput(68);
+
+        // This is somehow TOGGLING...
+        // From my understanding, should only appear when held.
+
+        DrawBlockBasedOnInput(wKey, blockN, '^');
+        DrawBlockBasedOnInput(aKey, blockW, '<');
+        DrawBlockBasedOnInput(sKey, blockS, 'v');
+        DrawBlockBasedOnInput(dKey, blockE, '>');
+
+
 
         if (spacebar & KeyStateDown)
         {
             std::cout << "Space Detected. " << std::endl;
             running = false;
         }
-
     }
+
+    SetConsoleCursorPosition(
+        GetStdHandle(STD_OUTPUT_HANDLE),
+        { 0, 0 }
+    );
 
     std::cout << "Exiting..." << std::endl;
 
