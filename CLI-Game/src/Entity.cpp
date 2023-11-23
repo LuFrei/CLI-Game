63bdd2f1f68b73c14d8243a5fb423fcbeeefdd1b
LuFrei <32626445+LuFrei@users.noreply.github.com>
2023-11-23 10:47:39 -0500

    Added width & height, and added Constructor

--- CLI-Game/src/Entity.cpp
+++ CLI-Game/src/Entity.cpp
@@ -4,3 +4,8 @@ void Entity::Move(int x, int y) {
 	posX += x;
 	posY += y;
 }
+
+Entity::Entity(unsigned int width, unsigned int height) {
+	this->width = width;
+	this->height = height;
+}
--- CLI-Game/src/Entity.h
+++ CLI-Game/src/Entity.h
@@ -5,8 +5,9 @@
 /// Such as a playable or non-playable character, floors, walls, props
 /// </summary>
 class Entity {
-	private:
-		int posX, posY;
-	public:
-		void Move(int x, int y);
+public:
+	int x, y;
+	unsigned int width, height;
+	Entity(int x, int y, unsigned int width, unsigned int height);
+	void Move(int x, int y);
 };
