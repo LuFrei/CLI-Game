52fb4c8a6d09a90876d516e24b4012d1e8756bb6
LuFrei <32626445+LuFrei@users.noreply.github.com>
2023-12-19 17:03:05 -0500

    Fixed issue #15

--- CLI-Game/src/Character.cpp
+++ CLI-Game/src/Character.cpp
@@ -5,7 +5,7 @@ Character::Character() {
 	y = 0;
 	width = 1;
 	height = 1;
-	rend = new Graphics::Renderer(0, 0, 1, 1, 'P');
+	rend = new Graphics::Renderer(0, 0, 8, 4, 'P');
 }
 
 void Character::Move(int x, int y) {
