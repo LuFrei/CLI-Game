4b0b22b1d4c4dccfea1a63a454c00f2d22d72859
LuFrei <32626445+LuFrei@users.noreply.github.com>
2024-01-25 12:30:38 -0500

    Updated game loop to take advantage of recent changes

--- CLI-Game/src/main.cpp
+++ CLI-Game/src/main.cpp
@@ -1,68 +1,27 @@
 #include <iostream>
 #include <Windows.h>
-<<<<<<< HEAD
 #include <chrono>
-#include "Input.h"
-#include "Character.h"
-#include "Render.h"
-=======
 #include <bitset>
 #include "Game/Character.h"
 #include "CLGEngine/Input.h"
 #include "CLGEngine/Graphics/Render.h"
 #include "CLGEngine/Graphics/Screen.h"
+#include "CLGEngine/Time.h"
 
-
->>>>>>> f2e691b9b861c164de7ad20f97aa42d6f4f89a3d
 
 int main()
 {
     bool running = true;
-<<<<<<< HEAD
-    int count = 0;
-    int countsThisSec = 0;
-    int countsPerSec = 0;
-    
-=======
-
     CLGEngine::Screen screen = CLGEngine::Screen(120, 30);
->>>>>>> f2e691b9b861c164de7ad20f97aa42d6f4f89a3d
-    Character* player = new Character();
-    time_t now = time(0);
-
-
+    Character* player = new Character(2);
 
     while (running) {
-#pragma region Debug Info
-<<<<<<< HEAD
+        CLGEngine::Time::CalculateDeltaTime();
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 });
-
-        if (time(0) != now) {
-            // 1 second elapsed
-            now = time(0);
-            countsPerSec = countsThisSec;
-            countsThisSec = 0;
-        }
-
-        // Display cycle
-        count++;
-        countsThisSec++;
-        std::cout << "Running... Cycle: " << count << std::endl;
-        std::cout << "Time: " << now << std::endl;
-
-        std::cout << "Frames This Second: " << countsThisSec << std::endl;
-        std::cout << "Frames Per Second: " << countsPerSec << std::endl;
-
-
-=======
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0, 0 });;
->>>>>>> f2e691b9b861c164de7ad20f97aa42d6f4f89a3d
-
-#pragma endregion
 
 #pragma region Logic
 
-#pragma region Input
+    #pragma region Input
         // Init inputs
         Input::Input::InitActiveKeys(
             {
@@ -75,22 +34,22 @@ int main()
         );
 
         Input::Input::UpdateActiveKeys();
-#pragma endregion
+    #pragma endregion
 
 #pragma region Gameplay
 
         
 
-        if (Input::Input::GetKeyDownThisCycle(Input::KeyCode::Up)) {
+        if (Input::Input::GetKeyDown(Input::KeyCode::Up)) {
             player->Move(0, -1);
         }
-        if (Input::Input::GetKeyDownThisCycle(Input::KeyCode::Down)) {
+        if (Input::Input::GetKeyDown(Input::KeyCode::Down)) {
             player->Move(0, 1);
         }
-        if (Input::Input::GetKeyDownThisCycle(Input::KeyCode::Left)) {
+        if (Input::Input::GetKeyDown(Input::KeyCode::Left)) {
             player->Move(-2, 0);
         }
-        if (Input::Input::GetKeyDownThisCycle(Input::KeyCode::Right)) {
+        if (Input::Input::GetKeyDown(Input::KeyCode::Right)) {
             player->Move(2, 0);
         }
 
