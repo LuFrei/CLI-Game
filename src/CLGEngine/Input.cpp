c7dc1ce92ed853d83558d0a8388dfa88055d876c
LuFrei <32626445+LuFrei@users.noreply.github.com>
2024-05-15 15:21:54 -0400

    refactored Input; Now checks updates all Input. New internal Key State, where least significant bit reflects a change in the input state. Main was also updated to use new Input methods.

--- src/CLGEngine/Input.cpp
+++ src/CLGEngine/Input.cpp
@@ -5,73 +5,31 @@
 #include <vector>
 #include "Input.h"
 
+#define KS_DOWN 0x8000
+#define KS_CHANGED 1
 
 namespace Input {
-	std::map<KeyCode, bool> keyDownStates;
-	std::map<KeyCode, bool> keyPressedStates;
-	std::map<KeyCode, bool> keyReleasedStates;
-	 
-	enum KeyState {
-		KeyState_Toggled = 1,
-		KeyState_Down = 0x8000
-	};
+	int keys[254];
 
-	void Input::InitActiveKeys(std::vector<KeyCode> keys) {
-		for (KeyCode key : keys) {
-			keyDownStates.insert({key, 0});
-			keyPressedStates.insert({ key, 0 });
-			keyReleasedStates.insert({ key, 0 });
+	void Input::UpdateAllInputState() {
+		for(int i = 1; i < 254; i++){
+			int keyState = GetAsyncKeyState(i) & KS_DOWN;
+			keyState += ((keys[i] & KS_DOWN) != keyState);
+			keys[i] = keyState;
 		}
 	}
 
-	void Input::UpdateActiveKeys() {
-		// Temporarily cached keys for Up/Down catches before storing them in keyStates
-		std::map<KeyCode, bool> newStates;
-
-
-		// Get new states af of this logic cycle.
-		for (auto state : keyDownStates) {
-			newStates.insert({ state.first, GetKeyState(static_cast<int>(state.first)) & KeyState_Down});
-		}
-
-		// get Key changes signifiying a downed state.
-		for (auto state : newStates) {
-			keyPressedStates[state.first] = 0;
-			keyReleasedStates[state.first] = 0;
-			if(newStates[state.first] != keyDownStates[state.first]) {
-				if (newStates[state.first]) {
-					keyPressedStates[state.first] = 1;
-				} else {
-					keyReleasedStates[state.first] = 1;
-				}
-			}
-		}
-
-		// update Down states
-		keyDownStates = newStates;
+	bool Input::GetKeyPressed(KeyCode key){
+		return keys[key] & KS_DOWN;
 	}
 
-	bool Input::GetKeyDown(KeyCode key) {
-		return keyDownStates[key];
+	bool Input::GetKeyJustPressed(KeyCode key){
+		return GetKeyPressed(key) 
+			&& (keys[key] & KS_CHANGED);
 	}
 
-	bool Input::GetKeyDownThisCycle(KeyCode key) {		
-		return keyPressedStates[key];
+	bool Input::GetKeyJustReleased(KeyCode key){
+		return !GetKeyPressed(key) 
+			&& (keys[key] & KS_CHANGED);
 	}
-
-	bool Input::GetKeyUpThisCycle(KeyCode key) {
-		return keyReleasedStates[key];
-	}
-
-	// This is gross, disgusting, dirty, but I'm tired of playing Socrates on the best possible solution.
-	// I just want it to work.
-	
-	/// <summary>
-	/// stores input as bitmask to check state
-	/// </summary>
-	/// <param name="key"></param>
-	/// <returns>Short representation of bit mask The keycode coresponds to.</returns>
-	//short storeState(KeyCode key, bool down) {
-
-	//}
 }
