bb060dcd11011df56568dd9b64ed640fa3ec2eab
LuFrei <32626445+LuFrei@users.noreply.github.com>
2025-01-10 11:30:40 -0500

    removed some dead code

--- src/CLGEngine/Graphics/Screen.h
+++ src/CLGEngine/Graphics/Screen.h
@@ -4,7 +4,6 @@
 #include <vector>
 #include <map>
 #include "Block.h"
-#include "../TileMap.h"
 #include "../CORE/Rect.h"
 
 namespace CLGEngine {
@@ -18,9 +17,6 @@ private:
 	int _height;
 
 	CHAR_INFO* _data;
-	
-	// TODO: Remove this
-	TileMap* tileMap;
 
 	void ClearScreenData();
 	std::vector<Block*> _renderQueue; //RenderCache? We are less going through a queue and more storing a cache of blocks...
@@ -28,15 +24,9 @@ public:
 
 	Screen(int width, int height);
 
-	void SetSquareCells(bool isSquare); // TODO: Remove this once TileMap renderer is implemented
-
 	void Draw();
 
 	void AddToRenderQueue(Block* block);
 	void RemoveFromRenderQueue(Block* block);
-
-	// TODO: This needs to be out of here.
-	// 	     TileMap should be the one providing data to screen.
-	void SetTileMap(TileMap* map) {tileMap = map;};
 };
 }
