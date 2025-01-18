46f78ea26ebeac155d7f9eea2b0f48bf67712c9f
LuFrei <32626445+LuFrei@users.noreply.github.com>
2025-01-17 22:35:15 -0500

    implemented switch

--- src/Game/Entities/Switch.h
+++ src/Game/Entities/Switch.h
@@ -1,9 +1,24 @@
 #pragma once
 
+#include <vector>
 #include "../../CLGEngine/Entity.h"
+#include "../../CLGEngine/Components/Collider.h"
+#include "../../CLGEngine/Components/Renderers/BlockRenderer.h"
+#include "Door.h"
 
 class Switch
 : public CLGEngine::Entity {
 private:
+    CLGEngine::Collider* _col;
+    CLGEngine::BlockRenderer* _rend;
+    std::vector<Door*> _doors;
+    short _color;
 public:
+
+    Switch(std::vector<Door*> doors);
+    
+    void Update();
+    void OnCollision(CLGEngine::Entity* hit);
+
+    void Activate();
 };
