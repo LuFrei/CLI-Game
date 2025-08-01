164bcc14cd2f6ce81780e004ecbc16eef6fb9278
LuFrei <32626445+LuFrei@users.noreply.github.com>
2025-08-01 13:38:33 -0400

    Visual Text Array added and working

--- subproc/main.cpp
+++ subproc/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <Windows.h>
+#include <array>
 
 #include "../shared.h"
 #include "libclr.h"
 
+#define SCREEN_WIDTH 120
+#define SCREEN_HEIGHT 30
+
 int main(int argc, char* argv[]){
-    clr::Screen* screen = new clr::Screen(120, 30);
+    clr::Screen* screen = new clr::Screen(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     HANDLE hFMO = OpenFileMapping(
         FILE_MAP_READ,
@@ -26,16 +30,23 @@ int main(int argc, char* argv[]){
     // extracting to avoid doing ((Shared::PlayerData*)sharedData) 20000 times
     Shared::PlayerData* pData = (Shared::PlayerData*)sharedData;
 
-    clr::Block* testSquare = new clr::Block(2, 4);
-    testSquare->Fill({'A', WHITE});
-    screen->AddToRenderQueue(testSquare);
+    std::array<clr::Block, SCREEN_HEIGHT> textLines;
+
+    WCHAR characters[4] = {'A', 'B', 'C', 'D'};
+
+    for(int i = 0; i < SCREEN_HEIGHT; i++){
+        textLines[i] = clr::Block(5, 1);
+        textLines[i].y = i;
+        textLines[i].Fill({characters[i%4], WHITE});
+        screen->AddToRenderQueue(&textLines[i]);
+    }
+    // clr::Block* testSquare = new clr::Block(2, 4);
+    // testSquare->Fill({'A', WHITE});
+    // screen->AddToRenderQueue(testSquare);
     screen->Draw();
 
     while(true){
-        printf("Player X Position: %d\nPlayer Y Position: %d", 
-            pData->xPlayerPos, 
-            pData->yPlayerPos
-        );
+        screen->Draw();
     }
 
     CloseHandle(hFMO);
